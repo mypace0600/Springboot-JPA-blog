a755b920abde10ad467cbc6ddc5b8d76a94cb0d5
심현수 <mypace0600@jobkorea.co.kr>
2023-02-21 15:24:50 +0900

    http test 2

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -4,29 +4,30 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HttpControllerTest {
 	
 	@GetMapping("/test/http")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) {
+		return "get 요청 :"+ m.getId();
 	}
 	
 	@PostMapping("/test/http")
-	public String postTest() {
-		return "post 요청";
+	public String postTest(@RequestBody Member m) {
+		return "post 요청 :"+ m.getId() + m.getUsername() + m.getEmail();
 	}
 	
 	@PutMapping("/test/http")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청 :"+ m.getId() + m.getUsername() + m.getEmail();
 	}
 	
 	@DeleteMapping("/test/http")
-	public String deleteTest() {
-		return "delete 요청";
+	public String deleteTest(@RequestBody Member m) {
+		return "delete 요청"+ m.getId() + m.getUsername() + m.getEmail();
 	}
 
 }
