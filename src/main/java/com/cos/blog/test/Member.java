09ec466c60e72ced91f1d0a3a8de973b59538d63
심현수 <mypace0600@jobkorea.co.kr>
2023-02-22 10:05:03 +0900

    yaml test and jsp setting

--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -2,25 +2,17 @@ package com.cos.blog.test;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Member {
 	private int id;
 	private String username;
 	private String password;
 	private String email;
-
-	@Builder
-	public Member(int id, String username, String password, String email) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.email = email;
-	}
 
 }
