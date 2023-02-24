69119bed92947dd85b84a460388fa29f927d6a3f
심현수 <mypace0600@jobkorea.co.kr>
2023-02-24 16:44:24 +0900

    글목록 보기 및 paging 처리

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,16 +1,24 @@
 package com.cos.blog.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardApiService;
 
 @Controller
 public class BoardController {
+
+	@Autowired
+	private BoardApiService service;
 	@GetMapping({"","/"})
-	public String index(){
-		return "index";
+	public String index(Model model,@PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+		model.addAttribute("boards",service.getList(pageable));
+		return "index"; // @Controller의 경우 viewResolver 작동되어 해당 페이지로 model의 정보를 들고 이동함
 	}
 
 	@GetMapping("/board/saveForm")
@@ -18,5 +26,6 @@ public class BoardController {
 		System.out.println("");
 		return "board/saveForm";
 	}
+
 
 }
--- src/main/java/com/cos/blog/service/BoardApiService.java
+++ src/main/java/com/cos/blog/service/BoardApiService.java
@@ -1,7 +1,11 @@
 package com.cos.blog.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +24,9 @@ public class BoardApiService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+
+	public Page<Board> getList(Pageable pageable){
+		return boardRepository.findAll(pageable);
 	}
 }
